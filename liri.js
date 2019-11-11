7ddf2f227a5fd6cbfe14031ddc7a702b2513a496
Broc <broc.davis@yahoo.com>
2019-11-10 19:26:43 -0600

    added in files and pre code for liri.js for switch case for all inputs from user

---
+++ liri.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+userInput = process.argv;
+switch (userInput) {
+    case concert-this:
+        break;
+    case spotify-this-song:
+        break;
+    case movie-this:
+        break;
+    case do-what-it-says:
+        break;
+        default:
+            console.log("please try entering in again.")
+}
+
