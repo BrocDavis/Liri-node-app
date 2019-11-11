420c9e9a27ce99e60b647c729f2f1950d488ef86
Broc <broc.davis@yahoo.com>
2019-11-10 21:16:48 -0600

    tested user input to make sure taking correct names and console log comes out of the correct case switch for the command entered in and taking in the song/movie/concert

--- liri.js
+++ liri.js
@@ -1,16 +1,38 @@
 require("dotenv").config();
+let keys = require("./keys.js");
+let Spotify = require('node-spotify-api');
+let Axios = require('axios');
 
-userInput = process.argv;
-switch (userInput) {
-    case concert-this:
+
+userCommand = process.argv[2];
+userInput = process.argv.slice(3).join(" ");
+
+switch (userCommand) {
+    case "concert-this":
+        //node liri.js concert-this <artist/band name here>\
+        concertThis(userInput);
         break;
-    case spotify-this-song:
+    case "spotify-this-song":
+        // node liri.js spotify-this-song '<song name here>'
+        console.log("spotify this song selected")
         break;
-    case movie-this:
+    case "movie-this":
+        //node liri.js movie-this '<movie name here>'
+        console.log("movie this selected")
         break;
-    case do-what-it-says:
+    case "do-what-it-says":
+        //node liri.js do-what-it-says
+        console.log("do what it says selected")
         break;
-        default:
-            console.log("please try entering in again.")
+    default:
+        console.log("please try entering in again.");
+        break;
 }
 
+function concertThis(artistName){
+    console.log(artistName);
+}
+
+function spotifySong(){
+    let spotify = new Spotify(keys.spotify);
+}
