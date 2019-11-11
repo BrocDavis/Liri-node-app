7db32ccb382275765d4db96c680e4affe3006df0
Broc <broc.davis@yahoo.com>
2019-11-10 23:48:50 -0600

    added in movie this functionality with axios call to ombd api that puts in the movie and adds a default movie if none are entered. Default movie is Mr. nobody.

--- liri.js
+++ liri.js
@@ -15,38 +15,53 @@ switch (userCommand) {
     case "spotify-this-song":
         // node liri.js spotify-this-song '<song name here>'
         console.log("spotify this song selected")
+        spotifySong(userInput);
         break;
     case "movie-this":
         //node liri.js movie-this '<movie name here>'
-        console.log("movie this selected")
+        movieThis(userInput);
         break;
     case "do-what-it-says":
         //node liri.js do-what-it-says
         console.log("do what it says selected")
+        doWhatItSays(userInput);
         break;
     default:
         console.log("please try entering in again.");
         break;
 }
 
-function concertThis(artist){
+function concertThis(artist) {
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    console.log(response.data[0]);
-  })
-
+        function (response) {
+            let venues = [];
+            console.log(response.data[0]);
+        })
 }
 
-function spotifySong(songName){
+function spotifySong(songName) {
     let spotify = new Spotify(keys.spotify);
 
 }
 
-function moveThis(movieName){
-    console.log(movieName);
+function movieThis(movieName) {
+        //default movie to Mr Nobody if no movie is chosen
+        if(!movieName){movieName = "Mr. Nobody"}
+        axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movieName + "&type=movie").then(
+            function (response) {
+                console.log(`
+                Name: ${response.data.Title}
+                Released: ${response.data.Released}
+                IMDB rating: ${response.data.Ratings[0].Value}
+                Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
+                Country: ${response.data.Country}
+                Language: ${response.data.Language}
+                Plot: ${response.data.Plot}
+                Actors: ${response.data.Actors}
+                `);
+            })
 }
-
-function doWhatItSays(artistName){
+function doWhatItSays(artistName) {
     console.log(artistName);
 }
 
